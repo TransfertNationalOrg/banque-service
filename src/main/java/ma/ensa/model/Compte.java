abbcc98a78bae529b36fea39bde56bdf715c6e19
ambou-97 <ambroise.tine@gmail.com>
2022-01-10 01:14:13 +0100

    Models

---
+++ src/main/java/ma/ensa/model/Compte.java
@@ -0,0 +1,21 @@
+package ma.ensa.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private Long RIB;
+    private double solde;
+    private Date date;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+}
