705968e802d78e4aa0b9affde5bf3f1e0924e5a7
ambou-97 <ambroise.tine@gmail.com>
2022-01-12 14:44:30 +0100

    @RepositoryRestResource added

--- src/main/java/ma/ensa/repository/CompteBancaireRepository.java
+++ src/main/java/ma/ensa/repository/CompteBancaireRepository.java
@@ -1,7 +1,9 @@
 package ma.ensa.repository;
 import ma.ensa.model.CompteBancaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CompteBancaireRepository extends JpaRepository<CompteBancaire, Long> {
 
 }
